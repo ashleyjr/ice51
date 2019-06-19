4de67ba0efba943c09ff7193caead972de2e17bd
Ashley J. Robinson <ashley181291@gmail.com>
2019-06-19 21:59:56 +0100

    Added test to data mem

---
+++ verif/src/uart_send_many_adds.c
@@ -0,0 +1,25 @@
+__xdata unsigned char * __code data = 0x201;
+__xdata unsigned char * __code cont = 0x200;
+
+void main (void){        
+   unsigned char a,b,c,d,e,f,g,h,i;
+   a = 1;
+   b = 1;
+   c = a + b;                          while(0x01 & *cont); *data = c;
+   d = a + b + c;                      while(0x01 & *cont); *data = d; 
+   e = a + b + c + d;                  while(0x01 & *cont); *data = e; 
+   f = a + b + c + d + e;              while(0x01 & *cont); *data = f; 
+   g = a + b + c + d + e + f;          while(0x01 & *cont); *data = g; 
+   h = a + b + c + d + e + f + g;      while(0x01 & *cont); *data = h;
+   i = a + b + c + d + e + f + g + h;  while(0x01 & *cont); *data = i;
+   while(1); 
+}
+
+// Check Uart:
+// 0x02
+// 0x04
+// 0x08
+// 0x10
+// 0x20
+// 0x40
+// 0x80
