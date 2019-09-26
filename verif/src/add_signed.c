ca9fa274584f9e515af071ab74e7db95393e4b1b
Ashley J. Robinson <ashley181291@gmail.com>
2019-09-26 21:02:18 +0100

    Added signed test and update checker

---
+++ verif/src/add_signed.c
@@ -0,0 +1,23 @@
+#include "ice51.h"
+
+void main (void){       
+   char i;
+   for(i=-5;i<6;i++){
+      *data = i;
+      while(0x01 & *cont);
+   }
+   while(1); 
+}
+
+// Check Uart:
+// -5
+// -4
+// -3
+// -2
+// -1
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
--- verif/utils/output_checks.py
+++ verif/utils/output_checks.py
@@ -20,19 +20,38 @@ if "__main__" == __name__:
                 c =str(hex(ord(c)))[2:4]
                 g.write("1"+c.lower() + "\n")
 
-            # Dec
-            else:
-                if "//" in j:
-                    d = j.replace("//","")
-                    d = str(hex(int(d)))[2:4]
-                    g.write("1" + d + "\n")
+            # Negs Dec
+            elif ("//" in j) and ("-" in j):
+                d = j.replace("//","")
+                d = d.replace("-","")
+                d = str(bin(int(d))).split('b')[1]
+                while(len(d) < 8):
+                    d = "0" + d
+                p = 128
+                t = 0
+                for s in d:
+                    if "0" == s:
+                        t += p
+                    p = p >> 1
+                d = str(hex(t + 1))[2:4]
+                g.write("1" + d + "\n")
+
+            # Pos Dec
+            elif "//" in j:
+                d = j.replace("//","")
+                d = str(hex(int(d)))[2:4]
+                if len(d) < 2:
+                    d = "0" + d
+                g.write("1" + d + "\n")
 
             checks = checks + 1
 
         if "// Check Uart" in j:
             found = True
+
     while(checks < 128):
         g.write("000\n")
         checks = checks + 1
+
     f.close()
     g.close()
